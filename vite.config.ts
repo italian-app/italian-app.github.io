a730c412ae0a0d974ff3967cc8ba8fac845a77b7
Thomas Klijnsma <thomasklijnsma@gmail.com>
2026-05-23 16:06:04 +0200

    Fix GitHub Pages Actions deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [
     svelte({ preprocess: vitePreprocess() }),
     VitePWA({
